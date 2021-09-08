aaf8cba33105243861ee6bd7d8f22143545e5765
Nabusim <cristianjose@gmail.com>
2021-09-08 15:45:43 +0200

    Init commit + Auctions crud

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,45 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const auctionsTable = process.env.AUCTIONS_TABLE;
+
+async function createAuction(event, context) {
+  // const { title } = JSON.parse(event.body);
+  const { title } = event.body;
+  const now = new Date();
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+  };
+
+  try {
+    await dynamodb
+      .put({
+        TableName: auctionsTable,
+        Item: auction,
+      })
+      .promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
